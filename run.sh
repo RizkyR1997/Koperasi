3eb849aabf079e5069fc163f4d0088c25534c9bf
RizkyR1997 <ahmad.rizky.rhomadoni.wibicon@gmail.com>
2021-07-29 11:53:13 +0700

    /

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 DESTINATION=$1
 PORT=$2
 # clone Odoo directory
-git clone --depth=1 https://github.com/minhng92/odoo-13-docker-compose $DESTINATION
+git clone --depth=1 https://github.com/RizkyR1997/koperasi $DESTINATION
 rm -rf $DESTINATION/.git
 # set permission
 mkdir -p $DESTINATION/postgresql
